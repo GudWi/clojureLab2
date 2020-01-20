75b412be1e553113e80a89b388d9a73c80305731
GudWi <SergeyLev22@yandex.ru>
2020-01-21 00:13:47 +0300

    version 4.0

--- src/main/java/main.clj
+++ src/main/java/main.clj
@@ -3,13 +3,13 @@
 
 (def in (a/chan 10))
 (def out (a/chan 10))
-(def accumulated (set ()))
+;(def accumulated (set ()))
 
 (defn fill-in-chan [] (take 10 [1 "func" 16 9 3 3 :reset "clojure" 5 3]))
 (a/onto-chan in (fill-in-chan))
 
 (defn fun [in out]
-  (a/go-loop []
+  (a/go-loop [accumulated (set ())]
              (when-some [value (a/<! in)]
                (println "value" value)
                (when (not= value :reset)
@@ -18,14 +18,15 @@
                    (println "founded in accumulated values"))
                  (when (not (contains? accumulated value))
                    (println "not found in accumulated values")
-                   (def accumulated (conj accumulated value))
                    (a/>! out value))
                  )
                (when (= value :reset)
                  (println "reset")
-                 (def accumulated (set nil))
                  )
-               (recur))))
+               (recur
+                 (if (= value :reset) (set nil)
+                                      (if (contains? accumulated value) accumulated (conj accumulated value))))
+               )))
 
 (fun in out)
 
